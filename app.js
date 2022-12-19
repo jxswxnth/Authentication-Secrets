21d1bde892a47f069c33f4652d852a95471a2eed
jxswxnth <jaswanthsai40@gmail.com>
2022-12-19 23:11:41 +0530

    Cookies and Sessions

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 require("dotenv").config()
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 const port = process.env.PORT;
-
+const secretString = process.env.SECRET;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.set("view engine", "ejs");
+
+//after requiring 'session', we configured some values in session
+app.use(session({
+  secret: secretString,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.set({
   strictQuery: true
@@ -24,7 +37,15 @@ const userSchema = new mongoose.Schema({
   username: String,
   password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const secretSchema = new mongoose.Schema({
   secret: String
@@ -41,23 +62,17 @@ app.route("/login")
     res.render("pages/login");
   })
   .post((req, res) => {
-    User.findOne({
-      username: req.body.username
-    }, (err, doc) => {
-      if (!err) {
-        if (doc) {
-          bcrypt.compare(req.body.password, doc.password, function(err, result) {
-            if (result) {
-              Secret.find((err, allSecrets) => {
-                res.render("pages/secrets", {
-                  listOfSecrets: allSecrets
-                });
-              });
-            }
-          });
-        } else {
-          res.redirect("/login");
-        }
+    const user = User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
       }
     });
   });
@@ -71,19 +86,40 @@ app.route("/register")
       username: req.body.username
     }, (err, doc) => {
       if (!err && doc) {
-        res.send("User already exist");
+        console.log("User already exists");
+        res.redirect("/register");
       } else {
-        bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-          const newUser = User({
-            username: req.body.username,
-            password: hash
-          });
-          newUser.save();
-          res.redirect("/login");
+        // No need to use save(), since we're using passport-local-mongoose it takes care of saving user in db.
+        // In User.register() we're sending only username since password will be stored as salt and hash in db.
+        // User.register() saves user into our db.
+        User.register({
+          username: req.body.username
+        }, req.body.password, (err, user) => {
+          if (err) {
+            console.log(err);
+            res.redirect("/register");
+          } else {
+            passport.authenticate("local")(req, res, () => {
+              res.redirect("/secrets");
+            });
+          }
         });
       }
     });
   });
+
+app.route("/secrets")
+  .get((req, res) => {
+    if (req.isAuthenticated()) {
+      Secret.find((err, allSecrets) => {
+        res.render("pages/secrets", {
+          listOfSecrets: allSecrets
+        });
+      });
+    } else {
+      res.redirect("/login");
+    }
+  })
 
 app.route("/submit")
   .get((req, res) => {
@@ -103,7 +139,13 @@ app.route("/submit")
 
 app.route("/logout")
   .get((req, res) => {
-    res.redirect("/");
+    req.logout((err)=>{
+      if(err){
+        console.log(err);
+      } else{
+        res.redirect("/");
+      }
+    })
   })
 
 
