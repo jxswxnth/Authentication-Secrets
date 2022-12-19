e463c68a122ccf1bd6d4d01155e3f6bf99176f1a
jxswxnth <jaswanthsai40@gmail.com>
2022-12-19 20:45:22 +0530

    Hashing and Salting with bcrypt

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require("dotenv").config()
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose=  require("mongoose");
-const md5 = require("md5");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 const port = process.env.PORT;
@@ -14,14 +15,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.set("view engine", "ejs");
 
-mongoose.set({strictQuery: true});
+mongoose.set({
+  strictQuery: true
+});
 mongoose.connect("mongodb://localhost:27017/secrets1DB");
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String
 });
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
 const secretSchema = new mongoose.Schema({
   secret: String
@@ -37,20 +40,22 @@ app.route("/login")
   .get((req, res) => {
     res.render("pages/login");
   })
-  .post((req,res)=>{
+  .post((req, res) => {
     User.findOne({
-      username: req.body.username,
-      password: md5(req.body.password)
-    },(err,doc)=>{
-      if(!err){
-        if(doc){
-          Secret.find((err,allSecrets)=>{
-            res.render("pages/secrets",{
-              listOfSecrets: allSecrets
-            });
+      username: req.body.username
+    }, (err, doc) => {
+      if (!err) {
+        if (doc) {
+          bcrypt.compare(req.body.password, doc.password, function(err, result) {
+            if (result) {
+              Secret.find((err, allSecrets) => {
+                res.render("pages/secrets", {
+                  listOfSecrets: allSecrets
+                });
+              });
+            }
           });
-        }
-        else{
+        } else {
           res.redirect("/login");
         }
       }
@@ -61,18 +66,21 @@ app.route("/register")
   .get((req, res) => {
     res.render("pages/register");
   })
-  .post((req,res)=>{
-    User.findOne({username: req.body.username},(err,doc)=>{
-      if(!err && doc){
+  .post((req, res) => {
+    User.findOne({
+      username: req.body.username
+    }, (err, doc) => {
+      if (!err && doc) {
         res.send("User already exist");
-      }
-      else{
-        const newUser = User({
-          username: req.body.username,
-          password: md5(req.body.password)
+      } else {
+        bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+          const newUser = User({
+            username: req.body.username,
+            password: hash
+          });
+          newUser.save();
+          res.redirect("/login");
         });
-        newUser.save();
-        res.redirect("/login");
       }
     });
   });
@@ -81,22 +89,22 @@ app.route("/submit")
   .get((req, res) => {
     res.render("pages/submit");
   })
-  .post((req,res)=>{
+  .post((req, res) => {
     const newSecret = Secret({
       secret: req.body.secret
     });
     newSecret.save();
-    Secret.find((err,allSecrets)=>{
-      res.render("pages/secrets",{
+    Secret.find((err, allSecrets) => {
+      res.render("pages/secrets", {
         listOfSecrets: allSecrets
       });
     });
   });
 
 app.route("/logout")
-.get((req,res)=>{
-  res.redirect("/");
-})
+  .get((req, res) => {
+    res.redirect("/");
+  })
 
 
 app.listen(port, (req, res) => {
